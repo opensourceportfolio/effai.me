04976d4948066239e3c3980837b8ef0f9f7b222b
Leon Tager <leon.tager@outlook.com>
2016-03-13 21:36:46 -0400

    added browser sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,16 @@
+/* eslint no-var: [0] */
+/* eslint prefer-arrow-callback: [0] */
+
 var gulp = require('gulp');
+var plumber = require('gulp-plumber');
 var changed = require('gulp-changed');
 var babel = require('gulp-babel');
 var sourcemaps = require('gulp-sourcemaps');
-var watch = require('gulp-watch');
 var postcss = require('gulp-postcss');
-var cssnext = require('cssnext');
+var cssnext = require('postcss-cssnext');
 var cssNested = require('postcss-nested');
+var browserSync = require('browser-sync').create();
+var history = require('connect-history-api-fallback');
 
 gulp.task('default', ['compile', 'css', 'copy-font']);
 
@@ -24,23 +29,35 @@ gulp.task('css', function() {
   gulp.src(['src/css/**/*.css'])
     .pipe(changed('dist/css'))
     .pipe(postcss(processors))
-    .pipe(gulp.dest('dist/css'));
+    .pipe(gulp.dest('dist/css'))
+    .pipe(browserSync.stream());
 });
 
 gulp.task('compile', function() {
   return gulp.src(['src/**/*.js', '!src/jspm_packages/**/*.js'])
+    .pipe(plumber())
     .pipe(changed('dist'))
-    // .pipe(sourcemaps.init({
-    //   loadMaps: 'inline'
-    // }))
+    .pipe(sourcemaps.init({
+      loadMaps: 'inline'
+    }))
     .pipe(babel({
       presets: ['es2015'],
       plugins: ['transform-react-jsx'],
     }))
     .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest('dist'))
+    .pipe(browserSync.stream());
 });
 
-gulp.task('watch', function() {
-  watch('src/**/*.js', ['default']);
+gulp.task('dev', function() {
+  browserSync.init({
+    injectChanges: true,
+    server: {
+      baseDir: './',
+      middleware: [history()]
+    }
+  });
+
+  gulp.watch('src/**/*.js', ['compile']);
+  gulp.watch('src/**/*.css', ['css']);
 });
