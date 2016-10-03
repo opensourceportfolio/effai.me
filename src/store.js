4483c33c47e36118fbfe8a25656ec3d6efb141fd
Leon Tager <ltager@twitter.com>
2016-10-03 10:21:30 -0700

    completed hot reload

--- src/store.js
+++ src/store.js
@@ -5,14 +5,5 @@ import { userInput } from 'reducer/fi';
 export default function configureStore(initialState) {
   const store = createStore(userInput, initialState);
 
-  if (module.hot) {
-    // Enable Webpack hot module replacement for reducers
-    module.hot.accept('reducer/fi', () => {
-      const nextReducer = require('reducer/fi').default;
-
-      store.replaceReducer(nextReducer);
-    });
-  }
-
   return store;
 }
